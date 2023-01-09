0343312e43ed84d4d098c92a785b04336ed0d4b0
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-01-08 21:29:08 -0500

    Add hardcoded response for development

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,4 +1,7 @@
+import exampleResponse from './example-response.json';
+
 export default async function fetchWeather() {
+  return exampleResponse;
   const url = 'https://api.openweathermap.org/data/3.0/onecall?lat=40.321510&lon=-75.983050&appid={APIKEY}&units=imperial';
   const resp = await fetch(url);
   return await resp.json();
