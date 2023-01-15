83cffb24cc508d28e7e373510c45d636ad2daa20
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-01-15 18:41:04 -0500

    Output stub hourly data

--- src/components/Today/index.js
+++ src/components/Today/index.js
@@ -2,5 +2,14 @@ export default function Today({
   daily,
   hourly
 }) {
-  return <h2>Today</h2>;
+  return <>
+    <h2>Today</h2>
+    <ul>
+      {
+        hourly.map((data) => <li key={data.dt}>
+          {data.dt} {data.temp}
+        </li>)
+      }
+    </ul>
+  </>;
 }
