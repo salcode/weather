492c98ebb6b444d25b5377208d639cc2e90e4ee3
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-01-15 18:52:50 -0500

    Remove .App-header

--- src/App.js
+++ src/App.js
@@ -31,11 +31,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
       <WeatherReport
         data={state.weather}
       />
