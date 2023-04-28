e514aa084f5a32ef6e143f465448ba2cc0c90cba
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-04-28 15:47:04 -0400

    Update Week to use new Day component

--- src/components/Week/index.js
+++ src/components/Week/index.js
@@ -1,5 +1,17 @@
+import Day from "../Day";
+
 export default function Week({
   daily,
 }) {
-  return <h2>Week</h2>;
+
+  return <>
+    <h2>Week</h2>
+    <ul className="day-forecast-list">
+    {
+      daily.slice(1).map((data) => <li key={data.dt}>
+        <Day day={data} />
+      </li>)
+    }
+    </ul>
+  </>;
 }
