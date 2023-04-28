97a4623a3e299bffdca0ab9df2cbfb2502e226c9
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-04-28 14:29:33 -0400

    Read from WP REST endpoint with built prod
    Read from WP REST endpoint with built prod, while continuing to use
    test data when using npm start for development

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,8 +1,11 @@
 import exampleResponse from './example-response.json';
 
 export default async function fetchWeather() {
-  return exampleResponse;
-  const url = 'https://api.openweathermap.org/data/3.0/onecall?lat=40.321510&lon=-75.983050&appid={APIKEY}&units=imperial';
-  const resp = await fetch(url);
+  console.log(process.env);
+  // Return test data if no REST API path is defined.
+  if (! process.env.REACT_APP_WEATHER_REST_API_PATH) {
+    return exampleResponse;
+  }
+  const resp = await fetch(process.env.REACT_APP_WEATHER_REST_API_PATH);
   return await resp.json();
 };
