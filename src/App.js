4cecc1406c109c02810c1f934ddf00d2a0cc1b28
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-05-26 16:06:42 -0400

    Update weather data every 30 seconds

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
 } from 'react';
 import fetchWeather from './utils/fetchWeather';
 import reducer from './reducer';
+import useInterval from './hooks/useInterval';
 import WeatherReport from './components/WeatherReport';
 
 import './App.css';
@@ -27,6 +28,18 @@ function App() {
       }
     })();
   }, []);
+
+  useInterval(async () => {
+    try {
+      const weather = await fetchWeather();
+      dispatch({
+        type: 'setWeather',
+        value: weather,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }, 30000);
 
   return (
     <div className="App">
