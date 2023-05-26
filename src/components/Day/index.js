7dbc2c4dfa12cf3e8336dd182d5d030da4c75e1e
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-05-26 15:32:20 -0400

    Add Day component

---
+++ src/components/Day/index.js
@@ -0,0 +1,26 @@
+import Conditions from "../Conditions";
+import Precipitation from "../Precipitation";
+
+function getDayName(dt) {
+  const dow = new Date(dt * 1000).toLocaleString(
+    "en-US",
+    { weekday: "short" }
+  );
+  return dow;
+}
+
+export default function Day({
+  day,
+}) {
+  return <>
+    { getDayName(day.dt) + ' '}
+    <span>
+      {Math.round(day.temp.max)}&deg; / {Math.round(day.temp.min)}&deg;
+    </span> <Precipitation
+      probability={day.pop}
+      rain={day.rain ?? 0}
+      snow={day.snow ?? 0}
+    />
+    <Conditions conditions={day.weather} />
+  </>;
+}
