78165cd5a25b19533ef2765ed2c493ab738bb8fd
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-06-22 22:45:28 -0400

    Remove example response code

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,11 +1,4 @@
-import exampleResponse from './example-response.json';
-
 export default async function fetchWeather() {
-  console.log(process.env);
-  // Return test data if no REST API path is defined.
-  if (! process.env.REACT_APP_WEATHER_REST_API_PATH) {
-    return exampleResponse;
-  }
   const resp = await fetch(process.env.REACT_APP_WEATHER_REST_API_PATH);
   return await resp.json();
 };
